48395a34b92dbbf37048027e3f13c595671b3896
Keitoto <keitotooffice@gmail.com>
2025-06-02 17:09:39 +0200

    Add page heading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,11 @@ import { TrendLoaderFallback } from '@/app/dashboard/components/TrendLoaderFallb
 export default function DashboardPage() {
   return (
     <>
+      <section className="mb-8">
+        <h1 className="text-2xl font-semibold">Summary</h1>
+        <p className="text-gray-600">Welcome to your financial dashboard. Here you can track your income, expenses, savings, and investments.</p>
+      </section>
+
       <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
         <Suspense fallback={<TrendLoaderFallback />}>
           <TrendLoader type="income" />
@@ -23,6 +28,11 @@ export default function DashboardPage() {
         <Suspense fallback={<TrendLoaderFallback />}>
           <TrendLoader type="investment" />
         </Suspense>
+      </section>
+
+      <section className="flex justify-between items-center mb-8">
+        <h2 className="text-xl font-semibold">Transactions</h2>
+
       </section>
 
       <Suspense fallback={<TransactionListFallback />}>
